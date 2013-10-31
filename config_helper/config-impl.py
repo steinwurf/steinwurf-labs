181bd344dc2f594f685938f28f363a6b2177cc91
Peter Vingelmann <petya@master.com>
2013-10-31 02:56:45 +0100

    update config helper with new mkspecs

--- config_helper/config-impl.py
+++ config_helper/config-impl.py
@@ -130,12 +130,11 @@ msvc_mkspec    = ['cxx_msvc11_x86', 'cxx_msvc11_x64']
 gxx_mkspec     = ['cxx_gxx46_x86', 'cxx_gxx46_x64',
                   'cxx_gxx47_x86', 'cxx_gxx47_x64',
                   'cxx_gxx48_x86', 'cxx_gxx48_x64']
-cross_mskpec   = ['cxx_crosslinux_gxx46_x86', 'cxx_crosslinux_gxx46_x64',
-                  'cxx_raspberry_gxx47_arm']
-clang_mkspec   = ['cxx_clang30_x86', 'cxx_clang30_x64',
-                  'cxx_clang32_x86', 'cxx_clang32_x64',
-                  'cxx_clang33_x86', 'cxx_clang33_x64']
-llvm_mkspec    = ['cxx_apple_llvm42_x86', 'cxx_apple_llvm42_x64']
+cross_mskpec   = ['cxx_crosslinux_gxx46_arm',
+                  'cxx_crosslinux_gxx46_x86', 'cxx_crosslinux_gxx46_x64',
+                  'cxx_crosslinux_gxx47_mips', 'cxx_raspberry_gxx47_arm']
+clang_mkspec   = ['cxx_clang33_x86', 'cxx_clang33_x64']
+llvm_mkspec    = ['cxx_apple_llvm50_x86', 'cxx_apple_llvm50_x64']
 ios_apple_mkspec = ['cxx_ios50_apple_llvm42_armv7']
 ios_clang_mkspec = ['cxx_ios50_clang32_armv7']
 
