6f1cdc94c92d00986a4e0020da38e82da0a97f5b
Peter Vingelmann <petya@master.com>
2014-01-21 20:01:14 +0100

    Add new clang mkspecs

--- config_helper/config-impl.py
+++ config_helper/config-impl.py
@@ -136,7 +136,10 @@ cross_mskpec   = ['cxx_crosslinux_gxx46_arm',
                   'cxx_crosslinux_gxx46_x86', 'cxx_crosslinux_gxx46_x64',
                   'cxx_crosslinux_gxx47_mips', 'cxx_raspberry_gxx47_arm']
 clang_mkspec   = ['cxx_clang33_x86', 'cxx_clang33_x64',
-                  'cxx_clang34_x86', 'cxx_clang34_x64']
+                  'cxx_clang34_x86', 'cxx_clang34_x64',
+                  'cxx_clang34_address_sanitizer_x64',
+                  'cxx_clang34_memory_sanitizer_x64',
+                  'cxx_clang34_thread_sanitizer_x64']
 llvm_mkspec    = ['cxx_apple_llvm50_x86', 'cxx_apple_llvm50_x64']
 ios_apple_mkspec = ['cxx_ios50_apple_llvm42_armv7',
                     'cxx_ios50_apple_llvm50_armv7']
